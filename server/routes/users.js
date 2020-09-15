99c1cb4ea3f349aeddcc9a9e96ba61c83e7e62b9
Hupaul <hupaulcamacho@pursuit.org>
2020-09-15 12:13:15 -0400

    adjust formatting

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const users = require('express').Router();
-const { createUser, getAllUsers } = require('../queries/users')
-const { checkFirebaseToken } = require('../middleware/auth')
+const { createUser, getAllUsers } = require('../queries/users');
+const { checkFirebaseToken } = require('../middleware/auth');
+
 users.post('/', createUser);
 users.get('/', checkFirebaseToken, getAllUsers)
 
